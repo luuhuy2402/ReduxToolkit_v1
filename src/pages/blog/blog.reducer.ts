d0da8790e7dcd28e175490a2646931cba5213b59
luuhuy2402 <xeoxeo1895@gmail.com>
2025-03-20 21:17:45 +0700

    Display data in State to UI

--- src/pages/blog/blog.reducer.ts
+++ src/pages/blog/blog.reducer.ts
@@ -1,11 +1,13 @@
 import { createReducer } from "@reduxjs/toolkit";
 import { Post } from "../../types/blog.type";
+import { initalPostList } from "../../constants/blog";
 
 interface BlogState {
     postList: Post[];
 }
 const initalState: BlogState = {
-    postList: [],
+    //khởi tạo state
+    postList: initalPostList,
 };
 // Hàm builder callback dùng để xử lý action và cập nhật state
 const blogReducer = createReducer(initalState, (builder) => {});
--- src/pages/blog/components/PostList/PostList.tsx
+++ src/pages/blog/components/PostList/PostList.tsx
@@ -1,6 +1,10 @@
+import { useSelector } from "react-redux";
 import PostItem from "../PostItem";
+import { RootState } from "../../../../store";
 
 export default function PostList() {
+    // useSelector là một hook của react-redux, được sử dụng để lấy dữ liệu từ Redux store trong một functional component của React.
+    const postList = useSelector((state: RootState) => state.blog.postList);
     return (
         <div className="bg-white py-6 sm:py-8 lg:py-12">
             <div className="mx-auto max-w-screen-xl px-4 md:px-8">
@@ -14,7 +18,9 @@ export default function PostList() {
                     </p>
                 </div>
                 <div className="grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-2 xl:grid-cols-2 xl:gap-8">
-                    <PostItem />
+                    {postList.map((post) => (
+                        <PostItem post={post} key={post.id} />
+                    ))}
                 </div>
             </div>
         </div>
